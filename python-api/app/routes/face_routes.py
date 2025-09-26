f10e4df72553fb88390aa6796bc8803a6dac83eb
mz0018 <martinezhanzmenzi@gmail.com>
2025-09-26 14:58:32 +0800

    added isSaveLogs to python-api

--- python-api/app/routes/face_routes.py
+++ python-api/app/routes/face_routes.py
@@ -147,7 +147,8 @@ async def recognize_face(data: dict):
         "visitor": best_visitor,
         "similarity": best_sim,
         "timestamp": datetime.utcnow(),
-        "expiresAt": datetime.utcnow() + timedelta(hours=1)
+        "expiresAt": datetime.utcnow() + timedelta(hours=1),
+        "isSaveToLogs": False
     }
     await logs_collection.insert_one(log_entry)
 
